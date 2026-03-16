21eef2bd35b04b702eb5b9b47930b7a3b0408d28
Lelozitos <lm.fabre@hotmail.com>
2026-03-15 22:51:43 -0300

    feat: frontend init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className="app-container">
+			<h1>Vivae</h1>
+			<p>Viva experiências inesquecíveis.</p>
+		</div>
+	);
+}
+
+export default App;
