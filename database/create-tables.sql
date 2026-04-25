af22adf521fc4c94af505a84ea681f98db192003
lelegallas <lelegallas@yahoo.com.br>
2026-04-25 08:48:41 -0300

    feat: detalhamento DBs

--- database/create-tables.sql
+++ database/create-tables.sql
@@ -1,43 +1,60 @@
+/* MS AUTH */
+
 CREATE TABLE IF NOT EXISTS usuarios (
     id_usuario SERIAL NOT NULL PRIMARY KEY,
-    username VARCHAR(30),
     senha_hash VARCHAR(60),
     email VARCHAR(255),
     nome_completo VARCHAR(100)
+    CEP VARCHAR (8),
+    logradouro VARCHAR (100),
+    complemento VARCHAR (50)
 );
+
+/* MS CATALOGO DE PRODUTOS */
 
 CREATE TABLE IF NOT EXISTS produtos(
     id_produto SERIAL NOT NULL PRIMARY KEY,
     nome VARCHAR(20),
     valor FLOAT
 );
-
-CREATE TABLE IF NOT EXISTS reservas(
-    id_reserva SERIAL NOT NULL PRIMARY KEY,
-    id_usuario INT,
-    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
-    id_produto INT,
-    FOREIGN KEY (id_produto) REFERENCES produtos(id_produto),
-    data_reserva TIMESTAMP,
-    valor_total FLOAT
-);
-
 CREATE TABLE IF NOT EXISTS materiais(
     id_material SERIAL NOT NULL PRIMARY KEY,
     nome VARCHAR(20),
-    valor_compra FLOAT
+    valor_compra FLOAT,
+    fornecedor VARCHAR(20)
 );
 
 CREATE TABLE IF NOT EXISTS composicao_produtos(
-    id_composicao INT NOT NULL PRIMARY KEY,
     id_produto INT,
     FOREIGN KEY (id_produto) REFERENCES produtos(id_produto),
     id_material INT,
-    FOREIGN KEY (id_material) REFERENCES materiais(id_material)
+    FOREIGN KEY (id_material) REFERENCES materiais(id_material),
+    PRIMARY KEY (id_produto, id_material)
 );
 
-ALTER TABLE usuarios ADD endereco VARCHAR(200);
-ALTER TABLE usuarios DROP COLUMN username;
+/* MS CARRINHO/PEDIDOS */
+CREATE TYPE status_pedido AS ENUM ('carrinho', 'pago', 'enviado', 'entregue');
 
+CREATE TABLE IF NOT EXISTS pedidos(
+    id_pedido SERIAL NOT NULL PRIMARY KEY,
+    id_usuario INT,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
+    data_reserva TIMESTAMP,
+    valor_total FLOAT,
+    estado status_pedido
+)
+
+CREATE TABLE IF NOT EXISTS itensPedido(
+    id_pedido INT,
+    FOREIGN KEY (id_pedido) REFERENCES pedidos(id_pedido),
+    id_produto INT,
+    FOREIGN KEY (id_produto) REFERENCES produtos(id_produto),
+)
+
+
+
+<<<<<<< Updated upstream
 ALTER TABLE usuarios ADD data_criacao TIMESTAMP;
 ALTER TABLE usuarios ADD COLUMN IF NOT EXISTS role VARCHAR(20) DEFAULT 'user';
+=======
+>>>>>>> Stashed changes
