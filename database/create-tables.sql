72d4f0565c8ed92744329f7db768ad399ce0be95
Lelozitos <lm.fabre@hotmail.com>
2026-05-11 19:52:14 -0300

    refactor: update database schema and clean up table definitions

--- database/create-tables.sql
+++ database/create-tables.sql
@@ -2,29 +2,32 @@
 
 CREATE TABLE IF NOT EXISTS usuarios (
     id_usuario SERIAL NOT NULL PRIMARY KEY,
-    senha_hash VARCHAR(60),
+    nome_completo VARCHAR(100),
     email VARCHAR(255),
-    nome_completo VARCHAR(100)
-    CEP VARCHAR (8),
-    logradouro VARCHAR (100),
-    complemento VARCHAR (50)
+    senha_hash VARCHAR(60),
+    CEP VARCHAR(8),
+    logradouro VARCHAR(100),
+    complemento VARCHAR(50),
+    role VARCHAR(20) DEFAULT 'user',
+    data_criacao TIMESTAMP
 );
 
 /* MS CATALOGO DE PRODUTOS */
 
-CREATE TABLE IF NOT EXISTS produtos(
+CREATE TABLE IF NOT EXISTS produtos (
     id_produto SERIAL NOT NULL PRIMARY KEY,
     nome VARCHAR(20),
     valor FLOAT
 );
-CREATE TABLE IF NOT EXISTS materiais(
+
+CREATE TABLE IF NOT EXISTS materiais (
     id_material SERIAL NOT NULL PRIMARY KEY,
     nome VARCHAR(20),
     valor_compra FLOAT,
     fornecedor VARCHAR(20)
 );
 
-CREATE TABLE IF NOT EXISTS composicao_produtos(
+CREATE TABLE IF NOT EXISTS composicao_produtos (
     id_produto INT,
     FOREIGN KEY (id_produto) REFERENCES produtos(id_produto),
     id_material INT,
@@ -33,28 +36,22 @@ CREATE TABLE IF NOT EXISTS composicao_produtos(
 );
 
 /* MS CARRINHO/PEDIDOS */
+
 CREATE TYPE status_pedido AS ENUM ('carrinho', 'pago', 'enviado', 'entregue');
 
-CREATE TABLE IF NOT EXISTS pedidos(
+CREATE TABLE IF NOT EXISTS pedidos (
     id_pedido SERIAL NOT NULL PRIMARY KEY,
     id_usuario INT,
     FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
     data_reserva TIMESTAMP,
     valor_total FLOAT,
     estado status_pedido
-)
+);
 
-CREATE TABLE IF NOT EXISTS itensPedido(
+CREATE TABLE IF NOT EXISTS itensPedido (
     id_pedido INT,
     FOREIGN KEY (id_pedido) REFERENCES pedidos(id_pedido),
     id_produto INT,
     FOREIGN KEY (id_produto) REFERENCES produtos(id_produto),
-)
-
-
-
-<<<<<<< Updated upstream
-ALTER TABLE usuarios ADD data_criacao TIMESTAMP;
-ALTER TABLE usuarios ADD COLUMN IF NOT EXISTS role VARCHAR(20) DEFAULT 'user';
-=======
->>>>>>> Stashed changes
+    PRIMARY KEY (id_pedido, id_produto)
+);
